54ba47cd51479fa5843632c589cff918640c99f3
MicroRay620 <microray620@gmail.com>
2025-09-03 15:49:40 -0400

    added nvim-lint plugin

---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,16 @@
+return {
+    "mfussenegger/nvim-lint",
+    config = function()
+        require("lint").linters_by_ft = {
+            c = {"clang-tidy", "cppcheck"},
+            cpp = {"clang-tidy", "cppcheck"},
+            cs = {"dotnet-format"},
+            javascript = {"eslint_d"},
+            lua = {"luacheck"},
+            markdown = {"markdownlint"},
+            python = {"flake8", "mypy"},
+            sh = {"shellcheck"},
+            typescript = {"eslint_d"},
+        }
+    end
+}
