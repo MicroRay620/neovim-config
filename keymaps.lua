4768143557fd2db5e1f209a3b81e48393b6c1f63
MicroRay620 <microray620@gmail.com>
2025-09-10 11:45:23 -0400

    removed leader keymap file requirement

--- keymaps.lua
+++ keymaps.lua
@@ -3,7 +3,6 @@ require("keymaps.ctrl-keymaps") -- control keymaps
 require("keymaps.extra-keymaps") -- extra keymaps (like Tab) -- This will also include
 require("keymaps.fun-keymaps") -- function (F keys) keymaps 
 require("keymaps.home-keymaps") -- home (Home, Ins, Del, End) keymaps
-require("keymaps.leader-keymaps") -- leader keymaps (by default, in the ./init.lua, it's set to Spacebar
 require("keymaps.shift-keymaps") -- shift keymaps 
 require("keymaps.std-keymaps") -- standard keys (all non-capital, 26 English characterwise
 -- May add numbers and stuff like that, idk yet
