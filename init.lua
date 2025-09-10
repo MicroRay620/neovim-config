c3997166befae5622a753f4bbedaf115fce15352
MicroRay620 <microray620@gmail.com>
2025-09-10 11:44:31 -0400

    fixed issue where the leader keys don't work

--- init.lua
+++ init.lua
@@ -18,6 +18,59 @@ NoChoices = {
     nowait = true
 }
 
+
+-- This only works if I have in the init.lua
+-- DO NOT CHANGE THIS
+vim.g.maplocalleader = " "
+vim.g.mapleader = " " -- This sets the leader to spacebar
+
+
+-- Navigation Keymaps
+
+-- Letter Keys
+-- Up
+vim.keymap.set("n", "<leader>k", "<C-w>k", vim.tbl_extend("keep", NoChoices, {
+    silent = false,
+    desc = "moves to the window up"
+}))
+-- Left
+vim.keymap.set("n", "<leader>h", "<C-w>h", vim.tbl_extend("keep", NoChoices, {
+    silent = false,
+    desc = "moves to the window to the left"
+}))
+-- Right
+vim.keymap.set("n", "<leader>l", "<C-w>l", vim.tbl_extend("keep", NoChoices, {
+    silent = false,
+    desc = "moves the window to the right"
+}))
+-- Down
+vim.keymap.set("n", "<leader>j", "<C-w>j", vim.tbl_extend("keep", NoChoices, {
+    silent = false,
+    desc = "moves to the window down"
+}))
+
+-- Arrow Key Moving
+-- Up Arrow
+vim.keymap.set("n", "<leader><Up>", "<C-w>k", vim.tbl_extend("keep", NoChoices, { 
+    silent = false,
+    desc = "arrow key. moves the window up"
+}))
+-- Left Arrow
+vim.keymap.set("n", "<leader><Left>", "<C-w>h", vim.tbl_extend("keep", NoChoices, {
+    silent = false,
+    desc = "arrow key. moves window to the left"
+}))
+-- Right Arrow
+vim.keymap.set("n", "<leader><Right>", "<C-w>l", vim.tbl_extend("keep", NoChoices, {
+    silent = false,
+    desc = "arrow key. moves window to the right"
+}))
+-- Down Arrow
+vim.keymap.set("n", "<leader><Down>", "<C-w>j", vim.tbl_extend("keep", NoChoices, {
+    silent = false,
+    desc = "arrow key. moves window down"
+}))
+
 require('keymaps')
 require('config.lazy')
 require('config.lsp')
