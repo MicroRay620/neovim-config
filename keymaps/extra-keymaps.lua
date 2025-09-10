f5f66f5101d94467ca02d4f728efec86da9fb36d
MicroRay620 <microray620@gmail.com>
2025-09-10 14:01:09 -0400

    created keymap files

--- keymaps/extra-keymaps.lua
+++
@@ -1,5 +0,0 @@
-vim.keymap.set("n", "<Tab>", ":NvimTreeFocus<CR>", vim.tbl_extend("keep", NoChoices, {
-    silent = true,
-    desc = "focuses the NvimTree"
-}))
-
--- keymaps/fun-keymaps.lua
+++
@@ -1,6 +0,0 @@
--- These are for the function keys
-vim.keymap.set("n", "<F2>", ":Run<CR>", vim.tbl_extend("keep", NoChoices, {
-    silent = true,
-    desc = "runs the debugged code"
-}))
-
