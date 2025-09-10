b0aed8d53697ea5830545a387d73867725be1efb
MicroRay620 <microray620@gmail.com>
2025-09-10 11:32:02 -0400

    added better documentation to the color schemes and how to change the color scheme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,5 +1,6 @@
 return {
-    "Mofiqul/dracula.nvim",
+    -- This sets the color scheme 
+    "Mofiqul/dracula.nvim", -- Change this to the github like (anything after github.com/) and paste it here
     config = function()
         vim.cmd("colorscheme dracula")
     end,
