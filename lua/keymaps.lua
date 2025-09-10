a026681407d25326053d6fbebc4080aec09da8bc
MicroRay620 <microray620@gmail.com>
2025-09-10 14:01:27 -0400

    created keymap file

---
+++ lua/keymaps.lua
@@ -0,0 +1,9 @@
+require("keymaps.alt-keymaps") -- alt keymaps
+require("keymaps.ctrl-keymaps") -- control keymaps
+require("keymaps.extra-keymaps") -- extra keymaps (like Tab) -- This will also include
+require("keymaps.fun-keymaps") -- function (F keys) keymaps 
+require("keymaps.home-keymaps") -- home (Home, Ins, Del, End) keymaps
+require("keymaps.lead-keymaps") -- leader keymaps (default set to space)
+require("keymaps.shift-keymaps") -- shift keymaps 
+require("keymaps.std-keymaps") -- standard keys (all non-capital, 26 English characterwise
+-- May add numbers and stuff like that, idk yet
