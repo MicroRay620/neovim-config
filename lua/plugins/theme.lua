4aabb1586781a2835dfb4789523d6dc8df058e18
MicroRay620 <microray620@gmail.com>
2025-09-12 20:02:41 -0400

    made neovim transparent, dracula was just commented out

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,6 +1,7 @@
 return {
     -- This sets the color scheme 
-    "Mofiqul/dracula.nvim", -- Change this to the github like (anything after github.com/) and paste it here 
+    -- "Mofiqul/dracula.nvim",-- Change this to the github like (anything after github.com/) and paste it here 
+    "tribela/transparent.nvim",
     config = function()
         vim.cmd("colorscheme dracula")
     end,
