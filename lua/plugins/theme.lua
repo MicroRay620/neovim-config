94e291beabba765dce2f603273cf1d23df1b6168
MicroRay620 <microray620@gmail.com>
2025-09-12 21:01:22 -0400

    added better documentation for the color schemes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,6 +3,6 @@ return {
     -- "Mofiqul/dracula.nvim",-- Change this to the github like (anything after github.com/) and paste it here 
     "tribela/transparent.nvim",
     config = function()
-        vim.cmd("colorscheme dracula")
+        vim.cmd("colorscheme transparent") -- Change this to the theme name as well if you're changing it
     end,
 }
