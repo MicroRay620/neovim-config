b03ea4b04b115a0477479fcf6f6148b1009acb06
MicroRay620 <microray620@gmail.com>
2025-09-12 21:28:58 -0400

    fixed error with the transparency and dracula

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,8 +1,8 @@
 return {
     -- This sets the color scheme 
-    -- "Mofiqul/dracula.nvim",-- Change this to the github like (anything after github.com/) and paste it here 
-    "tribela/transparent.nvim",
+    "Mofiqul/dracula.nvim",-- Change this to the github like (anything after github.com/) and paste it here 
+    "tribela/transparent.nvim", -- remove this is you don't want transparency 
     config = function()
-        vim.cmd("colorscheme transparent") -- Change this to the theme name as well if you're changing it
+        vim.cmd("colorscheme dracula") -- Change this to the theme name as well if you're changing it
     end,
 }
