df4ac6a6a6db5904cbd76348442085e0e7bc184a
MicroRay620 <microray620@gmail.com>
2025-09-14 13:45:14 -0400

    added some themese to theme.lua

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,8 +1,13 @@
 return {
     -- This sets the color scheme 
+    -- Here are some other themese you can use if you don't want transparent or dracula
+    -- "catppuccin/nvim.nvim"
+    -- "navarasu/onedark.nvim"
     "Mofiqul/dracula.nvim",-- Change this to the github like (anything after github.com/) and paste it here 
     "tribela/transparent.nvim", -- remove this is you don't want transparency 
     config = function()
         vim.cmd("colorscheme dracula") -- Change this to the theme name as well if you're changing it
+        -- vim.cmd("colorscheme nvim")
+        -- vim.cmd("colorscheme onedark")
     end,
 }
