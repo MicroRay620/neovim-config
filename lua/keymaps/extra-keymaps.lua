99521dabb5c5699b4ecf4eeace3fef5209f46157
MicroRay620 <microray620@gmail.com>
2025-09-15 11:59:38 -0400

    Added hslens keybinds

--- lua/keymaps/extra-keymaps.lua
+++ lua/keymaps/extra-keymaps.lua
@@ -3,3 +3,12 @@ vim.keymap.set("n", "<Tab>", ":NvimTreeFocus<CR>", vim.tbl_extend("keep", NoChoi
     desc = "focuses the NvimTree"
 }))
 
+vim.keymap.set("n", "*", "*<Cmd>lua require('hslens').start()<CR>", vim.tbl_extend("keep", NoChoices, {
+    silent = true,
+    desc = "this open hslens"
+}))
+
+vim.keymap.set("n", "#", "<Cmd>noh<CR>", vim.tbl_extend("keep", NoChoices,{
+    silent = true,
+    desc = "this closes hslens"
+}))
