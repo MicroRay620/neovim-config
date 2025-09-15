d7e1f6eee188149cd868fb14b43b0fee918cb10c
MicroRay620 <microray620@gmail.com>
2025-09-15 09:18:39 -0400

    added colon to documentation

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,6 +1,6 @@
 return {
     -- This sets the color scheme 
-    -- Here are some other themese you can use if you don't want transparent or dracula
+    -- Here are some other themese you can use if you don't want transparent or dracula:
     -- "catppuccin/nvim.nvim"
     -- "navarasu/onedark.nvim"
     "Mofiqul/dracula.nvim",-- Change this to the github like (anything after github.com/) and paste it here 
