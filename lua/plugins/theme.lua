7896cde641549ee3253cfd0b93e157471e759a42
MicroRay620 <microray620@gmail.com>
2025-09-17 19:36:13 -0400

    Created transparent.lua

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -4,7 +4,6 @@ return {
     -- "catppuccin/nvim.nvim"
     -- "navarasu/onedark.nvim"
     "Mofiqul/dracula.nvim",-- Change this to the github like (anything after github.com/) and paste it here 
-    "tribela/transparent.nvim", -- remove this is you don't want transparency 
     config = function()
         vim.cmd("colorscheme dracula") -- Change this to the theme name as well if you're changing it
         -- vim.cmd("colorscheme nvim")
