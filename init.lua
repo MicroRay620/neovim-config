e3d0f8877146950349123fe8175260d44533586a
MicroRay620 <microray620@gmail.com>
2025-09-18 16:54:22 -0400

    made it so a new error symbol is used

--- init.lua
+++ init.lua
@@ -11,16 +11,29 @@ vim.api.nvim_create_autocmd("VimEnter", {
     vim.cmd("packadd termdebug")
   end,
 })
-
 -- Define NoChoices with only noremap and nowait
 NoChoices = {
     -- This is used in all the keymap files, you will find those in lua/keymaps/
     noremap = true,
     nowait = true
 }
+
+-- This adds diagnoistic to your neovim.
+-- Remove this if you don't want this
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = "├──",  -- You can customize the prefix symbol
+    spacing = 1,    -- Space between text and prefix
+    -- You can also use severity sorting etc.
+  },
+  signs = true,    -- Show signs in gutter (optional)
+  underline = true,
+  update_in_insert = false,  -- Avoid flickering while typing
+})
+
+
 -- Require files
 require('keymaps')
 require('config.lazy')
 require('config.lsp')
 require('base')
--- To remove the transparency remove `lua/plugins/transparent.lua` file
