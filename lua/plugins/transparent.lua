c63cb3827328d6ba7059b94d0431e7fce7abc78e
MicroRay620 <microray620@gmail.com>
2025-09-17 20:02:43 -0400

    Fixed bug where the tree wasn't transparent

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,3 +1,16 @@
 return {
-    "xiyaowong/transparent.nvim"
+  {
+    "xiyaowong/transparent.nvim",
+    config = function()
+      require("transparent").setup({
+        groups = {'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+            'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+            'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+            'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
+            'EndOfBuffer',},
+        extra_groups = { "NvimTreeNormal", "NvimTreeNormalNC" },
+      })
+    end
+  }
 }
+
